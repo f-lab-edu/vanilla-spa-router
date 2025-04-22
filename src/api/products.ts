c5e79e24f97df2d9e7acc0ffd25689b4c083c7ab
kyeongwxx <kyeongwxx@gmail.com>
2025-04-23 00:52:53 +0900

    feat : 상품 리스트 API 정의

---
+++ src/api/products.ts
@@ -0,0 +1,11 @@
+import { Product } from '../types/product';
+
+export const fetchProducts = async (): Promise<Product[]> => {
+  const response = await fetch('/api/products');
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch products');
+  }
+
+  return response.json();
+};
